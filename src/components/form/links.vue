0357ed9ab4c8788b45449fbee86d85db5f11efea
SakhriHoussem <s.houssem.e@gmail.com>
2020-03-28 00:51:35 +0100

    - validate links form

--- src/components/form/links.vue
+++ src/components/form/links.vue
@@ -1,50 +1,34 @@
 <template>
-    <ValidationObserver ref="links" immediate  >
-        <ValidationProvider name="links" rules="required" v-slot="{failedRules, invalid, valid }">
-            <b-form-group label="add your links:" label-for="links">
-                <b-form-tags
-                        tag-variant="info"
-                        :class="{ 'is-invalid' : invalid, 'is-valid': valid  }"
-                        input-id="links"
-                        separator=" ,;"
-                        :value="form.links"
-                        @input="updateLinks"
-                        class="mb-2"
-                        remove-on-delete
-                        required
-                        :tag-validator="tagValidator"
-                >
-                    <small v-for="(failedRule, type) in failedRules" :key="type" class="text-danger">{{ failedRule }}</small>
-                </b-form-tags>
-            </b-form-group>
-        </ValidationProvider>
-    </ValidationObserver>
+    <b-form-group label="add your links:" label-for="links">
+        <b-form-tags
+                tag-variant="info"
+                input-id="links"
+                separator=" ,;"
+                :value="form.links"
+                @input="updateLinks"
+                class="mb-2"
+                remove-on-delete
+                required
+                :tag-validator="tagValidator"
+        >
+        </b-form-tags>
+    </b-form-group>
 </template>
 
 <script>
-    import { mapState } from 'vuex'
-    import { extend } from 'vee-validate';
-    import { required } from 'vee-validate/dist/rules';
-    extend('required', required);
+    import {mapState}  from 'vuex'
     export default {
         name: 'links',
         computed: {
-            ...mapState([
-                'form'
-            ])
+            ...mapState(['form'])
         },
         methods: {
             tagValidator(tag) {
                 return new RegExp("^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$").test(tag)
+                // return Validator.value(tag).url();
             },
             updateLinks(e) {
-                // add validation
-                this.$refs.links.validate().then(success => {
-                    if (!success) {
-                        this.$refs.links.setErrors([]);
-                    }
-                    this.$store.commit('updateStateField', {field: 'links',value: e})
-                });
+                this.$store.commit('updateStateField', {field: 'links',value: e})
             }
         },
     }
