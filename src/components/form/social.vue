7122df6ba9ddff10ffb87dd7fba62c210a5e6cd6
SakhriHoussem <s.houssem.e@gmail.com>
2020-03-28 01:22:27 +0100

    validate social form using simple-vue-validate

--- src/components/form/social.vue
+++ src/components/form/social.vue
@@ -1,60 +1,80 @@
 <template>
     <b-row>
-            <b-col cols="12">
-                <label>add your Social media:</label>
-            </b-col>
-            <b-col cols="12" sm="6" md="12" lg="6" >
+        <b-col cols="12">
+            <label>add your Social media:</label>
+        </b-col>
+        <b-col cols="12" sm="5" md="12" lg="5" >
+            <b-form-select
+                    :class="{ 'is-invalid' : validation.hasError('socialMedia')}"
+                    :options="getSocialNetworksList"
+                    v-model="socialMedia"
+                    name="socialMedia"
+            ></b-form-select>
+            <small class="text-danger">{{ validation.firstError('socialMedia')}}</small>
+        </b-col>
+        <b-col cols="12" sm="7" md="12" lg="7">
+            <b-input-group class="mb-2">
                 <b-form-input
+                        :class="{ 'is-invalid' : validation.hasError('pseudo')}"
                         class="form-control"
                         id="pseudo"
                         type="text"
                         v-model="pseudo"
+                        name="pseudo"
                         required
                         placeholder="Enter your Pseudo"
                 ></b-form-input>
-            </b-col>
-            <b-col cols="12" sm="6" md="12" lg="6">
-                <b-input-group class="mb-2">
-                    <b-form-select :options="getSocialNetworksList" v-model="socialMedia"></b-form-select>
-                    <b-input-group-append>
-                        <b-button @click="addSocialNetwork" variant="info">
-                            <font-awesome-icon icon="plus"></font-awesome-icon>
-                        </b-button>
-                    </b-input-group-append>
-                </b-input-group>
-            </b-col>
+                <b-input-group-append>
+                    <b-button @click="addSocialNetwork" variant="info">
+                        <font-awesome-icon icon="plus"></font-awesome-icon>
+                    </b-button>
+                </b-input-group-append>
+            </b-input-group>
+            <small class="text-danger">{{ validation.firstError('pseudo')}}</small>
+        </b-col>
     </b-row>
 </template>
 
 <script>
-    import {mapGetters, mapState} from "vuex";
+    import {mapGetters} from "vuex";
+    import SimpleVueValidation from "simple-vue-validator";
+    const Validator = SimpleVueValidation.Validator;
 
     export default {
         name:'social',
         data () {
             return {
                 pseudo: '',
-                socialMedia: null
+                socialMedia: null,
             }
         },
         computed: {
-            ...mapState([
-                'sociaux'
-            ]),
             ...mapGetters([
                 'getSocialNetworksList',
             ])
         },
+        validators: {
+            socialMedia: function (value) {
+                return Validator.value(value).required();
+            },
+            pseudo: function (value) {
+                    return  Validator.value(value).required().regex('^[a-zA-Z0-9-_]+$', 'Must be a valid pseudo.')
+            }
+        },
         methods: {
             addSocialNetwork () {
-                //todo: add validation
-                this.$store.commit('appendStateField',
-                    { field: 'socialNetworks',
-                        value: {icon:   this.socialMedia,
-                            pseudo: this.pseudo,
-                            url:    this.getSocialNetworksList.find( item => item.value === this.socialMedia).url + this.pseudo
-                        }
-                    })
+                // add validation
+                this.$validate().then((success)=> {
+                    if (success) {
+                        this.$store.commit('appendStateField',
+                            { field: 'socialNetworks',
+                                value: {icon:   this.socialMedia,
+                                    pseudo: this.pseudo,
+                                    url:    this.getSocialNetworksList.find( item => item.value === this.socialMedia).url + this.pseudo
+                                }
+                            })
+                    }
+                });
             }
         }
     }
