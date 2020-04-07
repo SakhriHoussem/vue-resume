92c36093667b0f6cb2b8e430b642761e05f48376
SakhriHoussem <s.houssem.e@gmail.com>
2020-04-07 20:37:25 +0100

    add a print button fixed on the botton instead of header

--- src/components/container.vue
+++ src/components/container.vue
@@ -10,18 +10,81 @@
                 </b-row>
             </b-col>
         </b-row>
-    </b-container>
+        <b-button
+                @click="print"
+                v-b-tooltip.hover
+                :title="$t('toggles.print')"
+                variant="dark"
+                size="lg"
+                id="print-me"
+        >
+            <font-awesome-icon icon="file-pdf" />
+        </b-button>    </b-container>
 </template>
 
 <script>
     import appSidebar from './sideBar'
     import paper from './paper.vue'
+    import jsPDF from 'jspdf'
+    import html2canvas from 'html2canvas'
 
     export default {
         name: 'appContainer',
         components: {
             appSidebar,
             paper
+        },
+        methods: {
+            print () {
+                // change the default width of the paper
+                document.getElementById('paper').setAttribute("style","width:1000px");
+
+                const filename = this.$store.state.form.fullName + ' Resume.pdf';
+                const a4Width  = 210.0015555555555;  // paper Standard Width
+                const a4Height = 297.0000833333333; // paper Standard Height
+                const divWidth = document.getElementById('paper').offsetWidth;
+
+                // get paper color side
+                const paperSide = document.getElementById('paper-side');
+
+                // calculate the color side  height
+                const paperSideHeight = (a4Height / a4Width) * divWidth;
+
+                // change the default height of the paper 
+                paperSide.setAttribute("style","min-height:" + paperSideHeight + "px");
+
+                const paper = document.getElementById('paper');
+                const divHeight = paper.offsetHeight;
+
+                html2canvas(paper, {
+                    height: divHeight,
+                    width: divWidth,
+                    scale: 5,
+                    allowTaint: true,
+                    useCORS: true,
+                    logging:	false
+                }).then(canvas => {
+                    let pdf = new jsPDF('p', 'mm', 'a4');
+                    const width = pdf.internal.pageSize.getWidth();
+                    const height = (divHeight / divWidth) * width; // ratio
+                    pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', -2, 0, width, height);
+
+                    // pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', -2, 0, 211, pdf.internal.pageSize.getHeight());
+                    pdf.save(filename);
+
+                    // restore the default height and width
+                    paperSide.removeAttribute("style");
+                    document.getElementById('paper').removeAttribute("style");
+                });
+            },
         }
     }
 </script>
+
+<style>
+    #print-me {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+    }
+</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -28,18 +28,6 @@
                 <b-nav-item >
                     <locale-changer/>
                 </b-nav-item>
-                <b-nav-item>
-                <b-button
-                            @click="print"
-                            v-b-tooltip.hover
-                            :title="$t('toggles.print')"
-                            variant="dark"
-                            size="sm"
-                            block
-                    >
-                        <font-awesome-icon icon="file-pdf" />
-                    </b-button>
-                </b-nav-item>
             </b-navbar-nav>
         </b-collapse>
 
@@ -47,60 +35,10 @@
 </template>
 
 <script>
-    import jsPDF from 'jspdf'
-    import html2canvas from 'html2canvas'
     import localeChanger from './form/localeChanger'
 
     export default {
         name: 'appHeader',
         components: {localeChanger},
-        methods: {
-            print () {
-                // change the default width of the paper
-                document.getElementById('paper').setAttribute("style","width:1000px");
-                
-                const filename = this.$store.state.form.fullName + ' Resume.pdf';
-                const a4Width  = 210.0015555555555;  // paper Standard Width
-                const a4Height = 297.0000833333333; // paper Standard Height
-                const divWidth = document.getElementById('paper').offsetWidth;
-                const paperSideHeight = (a4Height / a4Width) * divWidth;
-
-                // get paper side
-                const paperSide = document.getElementById('paper-side');
-
-
-                // change the default height of the paper
-                paperSide.setAttribute("style","min-height:" + paperSideHeight + "px");
-
-                const paper = document.getElementById('paper');
-                const divHeight = paper.offsetHeight;
-
-                html2canvas(paper, {
-                    height: divHeight,
-                    width: divWidth,
-                    scale: 5,
-                    allowTaint: true,
-                    useCORS: true,
-                    logging:	false
-                }).then(canvas => {
-                    let pdf = new jsPDF('p', 'mm', 'a4');
-                    const width = pdf.internal.pageSize.getWidth();
-                    const height = (divHeight / divWidth) * width; // ratio
-                    pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', -2, 0, width, height);
-
-                    // pdf.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', -2, 0, 211, pdf.internal.pageSize.getHeight());
-                    pdf.save(filename);
-
-                    // restore the default height and width
-                    paperSide.removeAttribute("style");
-                    document.getElementById('paper').removeAttribute("style");
-                });
-            },
-        }
     }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
